3cae560aac6767cf3815c12574ff22ff0c64cf73
Saul Ceja <saulceja08@gmail.com>
2023-04-05 16:59:12 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(mondayActivity='go to the office'){
+    return `This Monday, I will ${mondayActivity}.`
+}
+mondayWork();
+
+function wrapAdjective(result = '*'){
+    return function(emphatic='a hard worker'){
+       let message = `You are`;
+       return `${message} ${result}${emphatic}${result}!`
+    }
+}
